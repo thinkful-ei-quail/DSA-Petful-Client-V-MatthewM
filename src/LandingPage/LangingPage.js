6d82a3f6f4d54c0b06b5ac0c67ce28c743608dee
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-10-02 14:50:44 -0700

    Created basic landing page

---
+++ src/LandingPage/LangingPage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default class LandingPage extends React.Component {
+
+  render() {
+    return (
+      <>
+        <div>
+          <h1>Petful</h1>
+        </div>
+        <div>
+          <img src='https://i.pinimg.com/originals/12/05/a8/1205a86aa7226702b62569d0d0bb43fa.jpg' alt='The Petful Homepage' />
+        </div>
+        <div>
+          <p>Welcome to Petful! At Petful we follow an efficient and safe process to ensure that all pets get adopted.</p>
+          <p>
+            The adoption process is simple. Pets are put into an adoption queue when they arrive.
+            Adopters place their name at the end of the adoption queue, and get to adopt when their name is at the front.
+            Then adopters can only adopt the pet at the front of the queue for cats and/or dogs.
+            Adopters can adopt a Cat, a Dog, or one of each.
+          </p>
+        </div>
+        <div>
+          <input type='button' value='Adopt Now' />
+        </div>
+      </>
+    )
+  }
+}

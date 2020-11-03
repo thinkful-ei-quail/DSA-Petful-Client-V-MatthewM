850f921e242611c76e0e60edbcfed511995a30f3
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-11-03 10:54:55 -0800

    set endpoint

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    server_endpoint : 'http://localhost:8080/'
-    //server_endpoint : 'https://sleepy-sierra-29727.herokuapp.com/'
+    server_endpoint : 'https://sleepy-sierra-29727.herokuapp.com/'
+    //server_endpoint : 'http://localhost:8080/'
 }
